3b51ac56c0215791b43f9310fe11034ea909b6e9
Mark Veerasingam <markveer70@gmail.com>
2023-11-15 14:26:55 +0000

    first commit

---
+++ src/main/java/com/example/y3_lab8_cicd/Y3Lab8CicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.y3_lab8_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y3Lab8CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y3Lab8CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/y3_lab8_cicd/Y3Lab8CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.y3_lab8_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y3Lab8CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
